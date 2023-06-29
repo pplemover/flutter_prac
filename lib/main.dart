7bb1093ba88f80094d39bac238dd36f42b6e442b
Paul Lim <paulimbest@naver.com>
2023-06-29 17:59:37 +0900

    until_coolappmake

--- lib/main.dart
+++ lib/main.dart
@@ -46,18 +46,21 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(                             // Column은 Flutter에서 가장 기본적인 레이아웃 위젯 중 하나로, 하위 요소를 위에서 아래로 열에 배치합니다.
-        children: [
-          Text('A random idea:'),
-          BigCard(pair: pair),    // Text 위젯은 해당 클래스의 유일한 멤버인 current(즉, WordPair)에 액세스합니다.  
-
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(                             // Column은 Flutter에서 가장 기본적인 레이아웃 위젯 중 하나로, 하위 요소를 위에서 아래로 열에 배치합니다.
+          mainAxisAlignment: MainAxisAlignment.center,  // 기본(세로) 축을 따라 Column 내 하위 요소가 중앙에 배치됩니다.
+          children: [
+            Text('멋진 브랜드네임 생성기'),
+            BigCard(pair: pair),    // Text 위젯은 해당 클래스의 유일한 멤버인 current(즉, WordPair)에 액세스합니다.  
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -74,12 +77,19 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context); // 앱의 현재 테마를 확인하여 theme에 할당.
+    final style = theme.textTheme.displayMedium!.copyWith( // thme.textTheme을 사용하여 앱의 글꼴 테마에 액세스. 
+      color: theme.colorScheme.onPrimary, // onPrimary 속성은 앱의 기본 색상으로 사용하기 적합한 색상을 정의함.
+    );
 
     return Card(
       color: theme.colorScheme.primary, // 카드의 색상을 앱의 현재 테마와 동일하도록 정의함.
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}", // Text의 semanticsLabel 속성을 사용하여 텍스트 위젯의 시각적 콘텐츠를 스크린 리더에 더 적합한 시맨틱 콘텐츠로 재정의
+        ),
       ),
     );
   }
